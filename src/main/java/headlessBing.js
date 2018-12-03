e339017def29244029a831b7299abb369c079bfc
kumarsourav12 <kumarsourav.net@gmail.com>
2018-12-03 20:02:57 +0530

    few changes

--- src/main/java/headlessBing.js
+++ src/main/java/headlessBing.js
@@ -7,17 +7,26 @@ const headless = process.argv[2] === "headless";
     const browser = await puppeteer.launch({
         headless: headless,
         timeout,
+        args: ['--window-size=1360,768']
     });
     const page = await browser.newPage();
+    await page.setViewport({ width: 1360, height: 768 })
 	const label = "Go to Bing " + (headless?"without": "with") + " head."
 	console.time(label)
     await page.goto("https://www.bing.com/", { timeout })
 	const title = await page.title()
 	console.log("Title: " + title);
-	await page.waitForSelector("#scpl1");
-	await page.click("#scpl1");
-	const title2 = await page.title();
-    console.log("Title: " + title2);
+
+
+
+    await page.waitForSelector("#id_l");
+    await page.click("#id_1");
+
+    await page.waitForSelector("#i0116");
+    await page.type("#i0116","testuserName");
+const title3= await page.title();
+    console.log("Title: " + title3);
+
 	console.timeEnd(label);
     await browser.close();
 })();
